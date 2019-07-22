158f8653d11ed4fecbd11d21f71dcc77879a9b59
KiJun Park <ekdlwns3685@naver.com>
2019-07-22 17:15:05 +0900

    fixed clock

--- src/clock.js
+++ src/clock.js
@@ -4,8 +4,8 @@ function getTime() {
   let time = new Date();
   let hours = time.getHours();
   let minutes = time.getMinutes();
-  clock.childNodes[1].innerText = `${hours > 10 ? hours : `0${hours}`}:${
-    minutes > 10 ? minutes : `0${minutes}`
+  clock.childNodes[1].innerText = `${hours >= 10 ? hours : `0${hours}`}:${
+    minutes >= 10 ? minutes : `0${minutes}`
   }`;
 }
 
